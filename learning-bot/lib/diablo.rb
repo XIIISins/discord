6ce4a86837823ec67964413795829759b6471164
XIIISins <xiii@xiiisins.com>
2017-11-14 13:51:07 +0100

    start using modules

---
+++ learning-bot/lib/diablo.rb
@@ -0,0 +1,54 @@
+# diablo.rb
+module D3_Classes
+    diablo_classes = {
+    'Demon Hunter' => {
+        'dh' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        },
+    'Monk' => {
+        'mk' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        },
+    'Wizard' => {
+        'wz' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        },
+    'Barbarian' => {
+        'br' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        },
+    'Witch Doctor' => {
+        'wd' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        },
+    'Crusader' => {
+        'cs' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        },
+    'Necromancer' => {
+        'nm' => {
+            'beginner' => "url",
+            'intermediate' => "url",
+            'advanced' => "url"
+            }
+        }
+    }
+end
